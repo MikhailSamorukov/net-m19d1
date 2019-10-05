abe7a30688aa7bf668351c9312e4362f4dacfb92
rushella <zengoku.mj@gmail.com>
2019-10-05 21:32:51 +0600

    105192131

---
+++ M-01 Introduction to .NET/HelloLibrary/HelloClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloLibrary
+{
+    public static class HelloClass
+    {
+        public static string GenerateWelcomeString(DateTime dateTime, string name)
+        {
+            return string.Format($"[{dateTime}] Hello, {name}!");
+        }
+    }
+}
---
+++ M-01 Introduction to .NET/WindowsFormsApp/Main.cs
@@ -0,0 +1,21 @@
+﻿using HelloLibrary;
+using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void helloButton_Click(object sender, EventArgs e)
+        {
+            string userName = textBox1.Text;
+            string welcomeMessage = HelloClass.GenerateWelcomeString(DateTime.Now, userName);
+            MessageBox.Show(welcomeMessage);
+        }
+    }
+}
